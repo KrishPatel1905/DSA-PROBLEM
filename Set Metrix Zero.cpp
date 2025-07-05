a58eb2c3c8fae50444ee1554d8aafc77d17d8ad5
Krish patel <krishhpatel1905@gmail.com>
2025-07-05 18:51:36 +0530

    Leatcode

---
+++ Set Metrix Zero.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row, col;
+    cin >> row >> col;
+
+    int arr[row][col];
+
+   
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    
+    int rowMark[row] = {0};
+    int colMark[col] = {0};
+
+    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (arr[i][j] == 0) {
+                rowMark[i] = 1;
+                colMark[j] = 1;
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < row; i++) {
+        if (rowMark[i] == 1) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    for (int j = 0; j < col; j++) {
+        if (colMark[j] == 1) {
+            for (int i = 0; i < row; i++) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
