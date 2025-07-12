9b9077906611abe15d1af857b14e3ad8ca6bbbb8
Krish patel <krishhpatel1905@gmail.com>
2025-07-12 15:06:40 +0530

    CodeChef

---
+++ AvgNumber.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b,c;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>a>>b>>c;
+	    float d=(a+b)/2.0;
+	    if(c<d)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
