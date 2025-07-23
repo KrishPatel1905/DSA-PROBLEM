987e282d241be11dc3f231912eed29b389b86280
Krish patel <krishhpatel1905@gmail.com>
2025-07-23 18:16:50 +0530

    File Added

---
+++ CodeChef/Chef and Water Bottles.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	     int total=(c/b);
+	    
+	    
+	    if(total>a)
+	    {
+	        cout<<a<<endl;
+	    }
+	    else
+	    {
+	       cout<<total<<endl;     
+	      
+	    }
+	}
+
+}
