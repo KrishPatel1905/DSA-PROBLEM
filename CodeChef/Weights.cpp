43b898ce17d4709cb80d0ab41d6220f6489997aa
Krish patel <krishhpatel1905@gmail.com>
2025-08-20 20:04:42 +0530

    File Added

--- CodeChef/Weights.cpp
+++ CodeChef/Weights.cpp
@@ -8,9 +8,9 @@ int main() {
         int w, x, y, z;
         cin >> w >> x >> y >> z;
         if (w == x || w == y || w == z || w == x + y || w == x + z || w == y + z || w == x + y + z) {
-            cout << "YES" << endl;
+            cout << "YES." << endl;
         } else {
-            cout << "NO" << endl;
+            cout << "NO." << endl;
         }
     }
     return 0;         
