31b207e2a5b69d9ee641316f7bf2bffd603dbf30
Krish patel <krishhpatel1905@gmail.com>
2025-08-22 18:18:28 +0530

    Hackerrank

---
+++ Implement basic Operations(Push, pop and display)of stack using linked list.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct node {
+    int data;
+    struct node *link;
+} *first = NULL;
+
+
+struct node *create(int n) {
+    struct node *temp = new node();
+    temp->data = n;
+    temp->link = NULL;
+    return temp;
+}
+
+void push(int n) {
+    // int data;
+    // cin >> data;
+    struct node *temp = create(n);
+    temp->link = first;
+    first = temp;
+}
+void  pop() {
+    struct node *temp = first;
+    if (first == NULL) {
+    cout << "underflow"<<endl;
+        // return -1; 
+    } else {
+        int val = temp->data;  
+        first = first->link;
+        delete temp;           
+        
+    }
+}
+void display() {
+    struct node *temp = first;
+    if (first == NULL) {
+        cout << "underflow"<<endl;
+    } else {
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->link;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+     int choice;
+    do {
+        int x;
+        cin>>choice;
+        switch (choice) 
+        {
+            case 0:
+                return 0;
+            case 1:
+                cin>>x;
+                push(x);
+                break;    
+            case 2:
+                 pop();
+                break;
+            case 3:
+                display();
+                break;
+            
+        }
+    } while (choice != 0);
+    return 0;
+}
