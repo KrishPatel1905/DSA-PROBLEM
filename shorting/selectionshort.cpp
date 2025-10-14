04ff49a9b45db6dd79757a7658ec30d10a93d342
Krish patel <krishhpatel1905@gmail.com>
2025-10-14 21:39:30 +0530

    File Added

--- shorting/selectionshort.cpp
+++ shorting/selectionshort.cpp
@@ -5,7 +5,7 @@
     {
         int n;
         
-        cout<<"Enter the value of n ::";
+        cout<<"Enter the value of n :::";
         cin>>n;
         int arr[n];
         for(int i=0;i<n;i++)
